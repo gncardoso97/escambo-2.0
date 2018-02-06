95a3fd2c30b777e8211e65c3f39226e89aca59b6
gncardoso97 <gncardoso97@gmail.com>
2018-02-05 23:43:56 -0200

    Cadastrando servicos e usuarios

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('escambo', []);
+var app = angular.module('escambo', ['ngAnimate']);
 
 app.controller('ServicosController', function($scope, $http){
     $scope.modalInfo = {};
@@ -20,40 +20,56 @@ app.controller('ServicosController', function($scope, $http){
             $scope.modalInfo = servico;
         }
 
-    // $scope.dados = {};
-    // $scope.tarefas = [];
+})
 
-    // //atribuição de funções ao $scope
-    // $scope.inserirTarefa = function()
-    // {
-    //   var tarefa = {
-    //       nome: $scope.dados.nome,
-    //       feita: false
-    //   };
+app.controller('CadastroController', function($scope, $http){
+    $scope.showServico = false;
+    $scope.showUsuario = true;
+    $scope.nome = "";
 
-    //   if(tarefa.nome != ""){
-    //     $scope.tarefas.push(tarefa);
-    //     $scope.dados.nome = "";
-    // }
-    // };
+    $scope.salvarUsuario = function(usuario){
+        $scope.nome = usuario._id;
 
-    // $scope.fazerTarefa = function(tarefa)
-    // {
-    //   tarefa.feita = true;
-    // };
+        $http ({
+            method : "POST",
+            url :"http://45.55.82.101:3000/api/usuarios/criar",
+            data: usuario
+        }).then(function mySuccess (response){
+            $scope.showUsuario = false;
+            $scope.showServico = true;
+            console.log("response sucess: " + response);
+            $scope.servicos = response.data;
+            // console.log($scope.servicos);
+        }, 
+        function myError (response) {
+            // console.log(response.statusText);
+        });
+        
+    };
 
-    // $scope.limparTarefas = function()
-    // {
-    //   //Remove todas as tarefas
-    //   $scope.tarefas = [];
-    // };
+    $scope.salvarServico = function(servico){
+        
+        servico._id = $scope.usuario._id + "." + servico.nome.trim().replace(" ","");
+        servico.id_prestador = $scope.usuario._id;
+        servico.disponivel = true;
 
-    // $scope.apagar = function(tarefa)
-    // {
-    //   //pega o index da tarefa
-    //   var index = $scope.tarefas.indexOf(tarefa);
-    //    //Remove a tarefa selecionada
-    //   $scope.tarefas.splice(index, 1 );
-    // };
-
+        console.log("servico");
+        $http ({
+            method : "POST",
+            url :"http://45.55.82.101:3000/api/servicos/criar",
+            data: servico
+        }).then(function mySuccess (response){
+            $scope.showServico = false;
+            $scope.sucesso = true;
+            $scope.mensagem = "Serviço cadastrado com sucesso!";
+            console.log("response servico: " + response);
+            $scope.servicos = response.data;
+            // console.log($scope.servicos);
+        }, 
+        function myError (response) {
+            // console.log(response.statusText);
+        });
+        
+    };
+    
 });
